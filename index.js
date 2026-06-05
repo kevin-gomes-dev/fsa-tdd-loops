ebfaef9ea2a7eccd22780490dfa3ca59c514b7ae
Kevin Gomes <kevin.gomes.dev@gmail.com>
2026-06-04 22:47:38 -0400

    initial solution

--- index.js
+++ index.js
@@ -11,8 +11,17 @@
  * echo("test", 1); // "test"
  */
 export function echo(word, n) {
-  // TODO
+  let result = "";
+  for (let i = 0; i < n; i++) {
+    result += word;
+  }
+  return result;
 }
+
+console.log("----- echo -----");
+console.log(echo("bird", 3));
+console.log(echo("a", 5));
+console.log(echo("test", 1));
 
 /**
  * @param {string} word - The word to repeat.
@@ -27,8 +36,16 @@ export function echo(word, n) {
  * echoWithSpace("test", 1); // "test"
  */
 export function echoWithSpace(word, n) {
-  // TODO
+  let result = "";
+  for (let i = 0; i < n; i++) {
+    result += word + " ";
+  }
+  return result;
 }
+console.log("----- echoWithSpace -----");
+console.log(echoWithSpace("badger", 3));
+console.log(echoWithSpace("a", 2));
+console.log(echoWithSpace("test", 1));
 
 /**
  * @param {number} n - The number to stop at
@@ -42,8 +59,16 @@ export function echoWithSpace(word, n) {
  * sumTo(100); // 5050
  */
 export function sumTo(n) {
-  // TODO
+  let result = 0;
+  for (let i = 1; i <= n; i++) {
+    result += i;
+  }
+  return result;
 }
+console.log("----- sumTo -----");
+console.log(sumTo(5));
+console.log(sumTo(1));
+console.log(sumTo(100));
 
 /**
  * @param {number} a - The starting integer.
@@ -58,8 +83,16 @@ export function sumTo(n) {
  * sumFromTo(2, 2); // 2
  */
 export function sumFromTo(a, z) {
-  // TODO
+  let result = 0;
+  for (let i = a; i <= z; i++) {
+    result += i;
+  }
+  return result;
 }
+console.log("----- sumTo -----");
+console.log(sumFromTo(1, 3));
+console.log(sumFromTo(5, 7));
+console.log(sumFromTo(2, 2));
 
 /**
  * Counts down from n to 1, inclusive, by logging each number to the console.
@@ -73,8 +106,16 @@ export function sumFromTo(a, z) {
  * countdown(5); // logs 5, 4, 3, 2, 1
  */
 export function countdown(n) {
-  // TODO
+  for (let i = n; i >= 1; i--) {
+    console.log(i);
+  }
 }
+console.log("----- sumTo -----");
+countdown(3);
+console.log("--");
+countdown(1);
+console.log("--");
+countdown(5);
 
 /**
  * @param {number} n - The ending number.
@@ -88,8 +129,17 @@ export function countdown(n) {
  * sumOddsToN(1); // 1
  */
 export function sumOddsToN(n) {
-  // TODO
+  let result = 0;
+  // No need for % since adding 2 to an odd number will always get an odd number
+  for (let i = 1; i <= n; i += 2) {
+    result += i;
+  }
+  return result;
 }
+console.log("----- sumOddsToN -----");
+console.log(sumOddsToN(5));
+console.log(sumOddsToN(10));
+console.log(sumOddsToN(1));
 
 /**
  * A petri dish starts with a certain number of bacteria cells.
@@ -108,8 +158,19 @@ export function sumOddsToN(n) {
  * getGrowthTime(5, 78); // 80
  */
 export function getGrowthTime(start, target) {
-  // TODO
+  // If it's 1, multiply 2 and add 20 mins. 2, mul 2 add 20 (4 and 40). 4, mul 2 add 20 (8 and 60)
+  if (start <= 0) return;
+  let result = 0;
+  for (let i = start; i < target; i *= 2) {
+    result += 20;
+  }
+  return result;
 }
+console.log("----- getGrowthTime -----");
+console.log(getGrowthTime(0, 1337));
+console.log(getGrowthTime(1, 8));
+console.log(getGrowthTime(2, 78));
+console.log(getGrowthTime(5, 78));
 
 /**
  * The amount of money in a savings account grows by a certain rate every year.
@@ -128,8 +189,22 @@ export function getGrowthTime(start, target) {
  * getCompoundTime(30000, 0.04, 50000); // 14
  */
 export function getCompoundTime(start, rate, target) {
-  // TODO
+  if (start <= 0 || rate <= 0) return;
+  // at 100, add 100 * 0.1 (10%) which is 110, then add 110 * 0.1 which is 11, 121 + 12.1, 132.1 + 13.21...
+  let result = start;
+  let i = 0;
+  // While the result is less than the target, add to it the value of what it is * the rate
+  while (result < target) {
+    result *= 1 + rate;
+    i += 1;
+  }
+  return i;
 }
+// What would be cooler is a function that gives you your amount in x years instead of years for x target
+console.log("----- getCompoundTime -----");
+console.log(getCompoundTime(100, 0.1, 200));
+console.log(getCompoundTime(2000, 0.05, 5000));
+console.log(getCompoundTime(30000, 0.04, 50000));
 
 /**
  * An empty bucket sits some distance away from a water faucet.
@@ -154,8 +229,21 @@ export function getCompoundTime(start, rate, target) {
  * moveWater(7, 3); // 1
  */
 export function moveWater(colander, bucket) {
-  // TODO
+  // 5 and 18, first trip give 5, then next trip give 4 ... until 1, then repeat 1. Return trip count. Way to shorten code?
+  let tripCount = 0;
+  let currentBucketCups = 0;
+  let currentColander = colander;
+  while (currentBucketCups < bucket) {
+    currentBucketCups += currentColander;
+    currentColander = Math.max(currentColander - 1, 1);
+    tripCount++;
+  }
+  return tripCount;
 }
+console.log("----- moveWater -----");
+console.log(moveWater(5, 18));
+console.log(moveWater(5, 11));
+console.log(moveWater(7, 3));
 
 /**
  * This is a classic interview question! :)
@@ -174,5 +262,24 @@ export function moveWater(colander, bucket) {
  * fizzbuzz(15); // logs 1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz
  */
 export function fizzbuzz(n) {
-  // TODO
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
 }
+
+console.log("----- fizzbuzz -----");
+fizzbuzz(6);
+console.log("--");
+fizzbuzz(3);
+console.log("--");
+fizzbuzz(15);
+console.log("--");
+fizzbuzz(30);
